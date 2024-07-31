3821cdd498e0aa1887da17a930c2fa7dc1e81bd7
Kesasindar <142547094+Xyz4234@users.noreply.github.com>
2024-07-31 21:13:32 +0100

    Update chatbotapp2.py

--- chatbotapp2.py
+++ chatbotapp2.py
@@ -19,7 +19,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Wassup write smth here"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
